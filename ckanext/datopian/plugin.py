c03acbaf618feca5d775445cec4cd8b40981611c
Administrator <admin@example.com>
2024-09-25 14:30:28 +0000

    Update plugin.py

--- ckanext/datopian/plugin.py
+++ ckanext/datopian/plugin.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from flask import Blueprint, render_template
@@ -10,9 +11,9 @@ def hello_plugin():
 class DatopianPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.IFacets, inherit=True)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
@@ -20,7 +21,6 @@ class DatopianPlugin(plugins.SingletonPlugin):
                              'datopian')
 
     # IBlueprint
-
     def get_blueprint(self):
         u'''Return a Flask Blueprint object to be registered by the app.'''
         # Create Blueprint for plugin
@@ -29,3 +29,40 @@ class DatopianPlugin(plugins.SingletonPlugin):
         # Add plugin url rules to Blueprint object
         blueprint.add_url_rule('/hello_plugin', '/hello_plugin', hello_plugin)
         return blueprint
+
+    # IFacets
+    def dataset_facets(self, facets_dict, package_type):
+
+        if package_type != 'dataset':
+            return facets_dict
+
+        return OrderedDict([('organization', 'Unit Kerja'),
+                            ('groups', 'Kelompok'),
+                            ('tags', 'Tag'),
+                            ('res_format', 'Format'),
+                            ])
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+
+        if not package_type:
+            return OrderedDict([('organization', 'Unit Kerja'),
+                                ('groups', 'Kelompok'),
+                                ('tags', 'Tag'),
+                                ('res_format', 'Format'),
+                                ])
+        else:
+            return facets_dict
+
+    def group_facets(self, facets_dict, group_type, package_type):
+
+        # get the categories key
+        group_id = plugins.toolkit.c.group_dict['id']
+        key = 'vocab___category_tag_%s' % group_id
+        if not package_type:
+            return OrderedDict([('organization', 'Unit Kerja'),
+                                ('groups', 'Kelompok'),
+                                ('tags', 'Tag'),
+                                ('res_format', 'Format'),
+                                ])
+        else:
+            return facets_dict
